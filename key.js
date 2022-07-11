0180070d07a76d321e62c2bb8a8ba189a5aaa6bd
Leaf8910 <100060220+Leaf8910@users.noreply.github.com>
2022-07-12 06:47:51 +0800

    Update key.js
    Added key

--- key.js
+++ key.js
@@ -1,4 +1,7 @@
 var config = { 
     key : "6e69e26d2dbce83a2cd91866c8eab755",
     unsplash_key : "kkjYIW9jODxwsCbheM7bUT8giS9W9qwTwc49vmRyZiM",
+    ip_key : "3fd94bdb28ea4dbf8403fc435aecf14f",
+    geocode : "AIzaSyBjgwSnLej2DMeGIT9ovpgZg1XfxDIaAkY",
+    openCage : "14f2b94f3c214fc1825be7d445737e77"
 }
